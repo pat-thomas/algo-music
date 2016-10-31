2b25b6a7a44c0c31b443c3f8c55a211c0a036c17
pat-thomas <patthomassoftware@gmail.com>
2016-10-30 20:30:06 -0400

    scratch code

--- am/src/am/core.clj
+++ am/src/am/core.clj
@@ -1,6 +1,30 @@
-(ns am.core)
+(ns am.core
+  (:import [javax.sound.sampled Clip AudioInputStream AudioSystem]
+           [java.io File]))
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+(defn make-sound
+  [filepath]
+  (let [sound-file (File. filepath)
+        sound      (AudioSystem/getAudioInputStream sound-file)
+        sound-clip (AudioSystem/getClip)]
+    (.open sound-clip sound)
+    (.setFramePosition sound-clip 0)
+    (.start sound-clip)))
+
+(comment
+  (let [filepath "/Users/pthomas/Downloads/[99Sounds] 99 Drum Samples/Samples/kick-oldschool.wav"]
+    (dotimes [_ 16]
+      (make-sound filepath)
+      (Thread/sleep 100))))
+
+(comment
+  ;; TOOD: make this work
+  
+  (defcomp test-comp
+    e-major ;; scale
+    {:times [4] :variation 2} ;; each note will be able to go at most 2 whole steps up or down from note at beginning of grouping)
+    {:times [2] :variation 3})
+
+  )
+
+
